5c642b4cbb5ad3e343850975024417fd8c569710
Steven Treadway <treadway08@gmail.com>
2016-05-31 18:59:23 -0500

    adds document type to document type page

--- src/public/controllers/DocumentTypeCtrl.js
+++ src/public/controllers/DocumentTypeCtrl.js
@@ -1,4 +1,4 @@
 angular.module('PMApp').controller('DocumentTypeCtrl', function ($scope, $stateParams) {
   $scope.id = $stateParams.id
-  $scope.type = 'TYPE'
+  $scope.type = $stateParams.type
 })
